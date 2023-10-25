24c333e5d4dda71ba200d73d746d018167fca24d
Hugo Marques <43884516+hggmarks@users.noreply.github.com>
2023-10-25 17:28:00 -0300

    feat(evaluator): added evaluator for numeric operations and die roll

--- src/parsedie/ast.rs
+++ src/parsedie/ast.rs
@@ -1,3 +1,8 @@
+use rand::Rng;
+use rand::rngs::OsRng;
+use std::error;
+
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Node {
     Add(Box<Node>, Box<Node>),
@@ -9,3 +14,108 @@ pub enum Node {
     Die(Box<Node>, Box<Node>),
     Number(i128),
 }
+
+#[derive(Clone, Debug, PartialEq)]
+enum EvalResult {
+    Number(f64),
+    DieResult(Vec<i128>),
+}
+
+
+pub fn eval(expr: Node) -> Result<EvalResult, Box<dyn error::Error>> {
+    match expr {
+        Node::Number(i) => Ok(EvalResult::Number(i as f64)),
+        Node::Add(expr1, expr2) => {
+
+            let lhs = eval(*expr1)?;
+            let rhs = eval(*expr2)?;
+            
+            match (lhs, rhs) {
+                (EvalResult::Number(num1), EvalResult::Number(num2)) => {
+                    Ok(EvalResult::Number(num1 + num2))
+                }
+                _ => Err("Cannot Add DieResult to Number".into()),
+
+            }
+        }
+        Node::Subtract(expr1, expr2) => {
+
+            let lhs = eval(*expr1)?;
+            let rhs = eval(*expr2)?;
+            
+            match (lhs, rhs) {
+                (EvalResult::Number(num1), EvalResult::Number(num2)) => {
+                    Ok(EvalResult::Number(num1 - num2))
+                }
+                _ => Err("Cannot Subtract DieResult to Number".into()),
+            }
+        }
+        Node::Multiply(expr1, expr2) => {
+            
+            let lhs = eval(*expr1)?;
+            let rhs = eval(*expr2)?;
+            
+            match (lhs, rhs) {
+                (EvalResult::Number(num1), EvalResult::Number(num2)) => {
+                    Ok(EvalResult::Number(num1 * num2))
+                }
+                _ => Err("Cannot Multiply DieResult by Number".into()),
+            }
+        }
+        Node::Divide(expr1, expr2) => {
+            
+            let lhs = eval(*expr1)?;
+            let rhs = eval(*expr1)?;
+
+            match (lhs, rhs) {
+                (EvalResult::Number(num1), EvalResult::Number(num2)) => {
+                    Ok(EvalResult::Number(num1 / num2))
+                }
+                _ => Err("Cannot Divide DieResult by Number".into())
+            }
+        },
+        Node::Negative(expr1) => {
+            let value = eval(*expr1)?;
+
+            match value {
+                EvalResult::Number(val) => {
+                    Ok(EvalResult::Number(-val))
+                }
+                _ => Err("DieResult cannot be negative".into())
+            }
+        }
+        Node::Caret(expr1, expr2) => {
+            let lhs = eval(*expr1)?;
+            let rhs = eval(*expr2)?;
+
+            match (lhs, rhs) {
+                (EvalResult::Number(num1), EvalResult::Number(num2)) => {
+                    Ok(EvalResult::Number(num1.powf(num2)))
+                }
+                _ => Err("Cannot use DieResult in power operation".into())
+            }
+        }
+        Node::Die(expr1, expr2) => {
+            let num_rows = eval(*expr1)?;
+            let num_sides = eval(*expr2)?;
+            let mut results: Vec<i128> = Vec::new();
+            let mut rng = OsRng;
+
+            if let (EvalResult::Number(num_rows), EvalResult::Number(num_sides)) = (num_rows, num_sides) {
+                if num_rows == 0.0 || num_sides == 0.0 {
+                    return Ok(EvalResult::DieResult(vec![0]));
+                }
+
+                for _ in 0..(num_rows as i128) {
+                    results.push(rng.gen_range(1..=(num_sides as i128)));
+                }
+
+                Ok(EvalResult::DieResult(results))
+            } else {
+                Err("Die expressions must have numeric operands".into())
+            }
+            
+        }   
+    }
+} 
+
